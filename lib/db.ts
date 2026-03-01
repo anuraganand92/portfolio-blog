157a73ae5eb8282541d2d7accccbdc8cc4749414
Anurag Anand <anuragsr26@gmail.com>
2026-03-01 23:47:55 +0530

    Add Data Mesh and Apache Iceberg blogs, fix db location to prevent parsing errors

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,26 @@
 import fs from 'fs';
 import path from 'path';
 
-const dbPath = path.join(process.cwd(), 'data', 'db.json');
+const dbPath = path.join(process.cwd(), 'lib', 'db.json');
 
 type DbSchema = {
-  likes: Record<string, number>;
-  hits: Record<string, number>;
-  subscribers: string[];
+    likes: Record<string, number>;
+    hits: Record<string, number>;
+    subscribers: string[];
 };
 
 export function getDb(): DbSchema {
-  if (!fs.existsSync(dbPath)) {
-    saveDb({ likes: {}, hits: {}, subscribers: [] });
-  }
-  const data = fs.readFileSync(dbPath, 'utf8');
-  try {
-      return JSON.parse(data);
-  } catch(e) {
-      return { likes: {}, hits: {}, subscribers: [] };
-  }
+    if (!fs.existsSync(dbPath)) {
+        saveDb({ likes: {}, hits: {}, subscribers: [] });
+    }
+    const data = fs.readFileSync(dbPath, 'utf8');
+    try {
+        return JSON.parse(data);
+    } catch (e) {
+        return { likes: {}, hits: {}, subscribers: [] };
+    }
 }
 
 export function saveDb(data: DbSchema) {
-  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
 }
